b0492a73c5fff077b73479749f54a2055d9acc32
Gabriel <gakirakiyohara@gmail.com>
2026-02-05 16:14:49 -0300

    fix route

--- src/routes/movimentacao.routes.js
+++ src/routes/movimentacao.routes.js
@@ -1,10 +1,5 @@
-import { listarProblemasInconsistencia } from "../controllers/movimentacaoController.js";
-router.get(
-  "/problemas-inconsistencia",
-  autenticar,
-  listarProblemasInconsistencia,
-);
 import express from "express";
+import { listarProblemasInconsistencia } from "../controllers/movimentacaoController.js";
 import {
   registrarMovimentacao,
   listarMovimentacoes,
@@ -20,6 +15,12 @@ import {
 } from "../middlewares/auth.js";
 
 const router = express.Router();
+// Corrigido: rota deve ser registrada após router ser inicializado
+router.get(
+  "/problemas-inconsistencia",
+  autenticar,
+  listarProblemasInconsistencia,
+);
 
 router.get("/", autenticar, listarMovimentacoes);
 router.get("/:id", autenticar, verificarPermissaoLoja(), obterMovimentacao);
